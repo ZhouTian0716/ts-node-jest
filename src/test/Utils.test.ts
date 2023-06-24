29c450f286ca8a321fe65a1026d48449b78d4b38
Zhou_Tian_MBP <egglord0716@gmail.com>
2023-06-24 10:14:48 +1000

    debug config ready

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,45 @@
-import { toUpper } from "../app/Utils";
+import { getStringInfo, StringUtils, toUpperCase } from "../app/Utils";
 
-describe("Utils", () => {
-  it("should pass", () => {
-    const res = toUpper("xxx");
-    expect(res).toBe("XXX");
+describe("Utils test suite", () => {
+  describe.only("StringUtils tests", () => {
+    let sut: StringUtils;
+
+    beforeEach(() => {
+      sut = new StringUtils();
+    });
+
+    afterEach(() => {
+      console.log("After each!");
+    });
+
+    it.only("Should return correct upperCase", () => {
+      const actual = sut.toUpperCase("abc");
+      expect(actual).toBe("ABC");
+    });
+
+    it("Should throw error on invalid argument - function", () => {
+      function expectError() {
+        const actual = sut.toUpperCase("");
+      }
+      expect(expectError).toThrow();
+      expect(expectError).toThrowError("Invalid argument!");
+    });
+
+    it("Should throw error on invalid argument - arrow function", () => {
+      expect(() => {
+        sut.toUpperCase("");
+      }).toThrowError("Invalid argument!");
+    });
+
+    it("Should throw error on invalid argument - try catch block", (done) => {
+      try {
+        sut.toUpperCase("");
+        done("GetStringInfo should throw error for invalid arg!");
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error).toHaveProperty("message", "Invalid argument!");
+        done();
+      }
+    });
   });
 });
